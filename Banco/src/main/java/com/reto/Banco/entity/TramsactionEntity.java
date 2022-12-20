ccfaa91004d20585df629ab31406d9c9505825b0
NessDisk <ness.cero1@gmail.com>
2022-12-20 18:46:23 -0500

    Entity table.

---
+++ Banco/src/main/java/com/reto/Banco/entity/TramsactionEntity.java
@@ -0,0 +1,21 @@
+package com.reto.Banco.entity;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import java.sql.Date;
+
+import javax.swing.Spring;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Transaction")
+public class TramsactionEntity {
+    
+}
